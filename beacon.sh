09551919665ae5e19c59eced31af2f2daad8a76e
PizzaDeliveryDude <garrett.oster@me.com>
2026-01-20 14:33:40 -0500

    remove beacon.sh

--- beacon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-Channel="${1:-0}"
-Beacon="${2:-I am a beacon}"
-
-#python -m venv bashvenv && source ~/src/venv/bin/activate;
-source meshtastic_venv/bin/activate;
-
-meshtastic --ch-index $Channel --sendtext "$Beacon"
-
